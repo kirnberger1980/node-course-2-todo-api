dbe37c2f45151b035260695374e85de78bf15fa5
Marcell Jobs <marcell.jobs@gmail.com>
2017-08-31 12:49:32 +0200

    Add test cases for GET /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,12 +1,15 @@
 const expect = require('expect');
 const request = require('supertest');
+const {ObjectID} = require('mongodb');
 
 const {app} = require('./../server.js');
 const {Todo} = require('./../models/todo.js');
 
 const todos = [ {
+  _id: new ObjectID(),
   text: 'First test todo'
 }, {
+  _id: new ObjectID(),
   text: 'Second test todo'
 }]
 
@@ -65,6 +68,36 @@ describe('POST /todos', () => {
           expect(res.body.todos.length).toBe(2);
         })
         .end(done);
-    })
+    });
+  });
+
+  describe('GET /todos/:id', () => {
+    it('should return todo doc',(done) => {
+      var id = todos[0]._id.toHexString();
+      request(app)
+        .get(`/todos/${id}`)
+        .expect(200)
+        .expect((res) => {
+          expect(res.body.todo.text).toBe(todos[0].text)
+        })
+        .end(done);
+    });
+
+    it('should return if todo not found',(done) => {
+      var id = new ObjectID().toHexString();
+      request(app)
+        .get(`/todos/${id}`)
+        .expect(404)
+        .end(done);
+    });
+
+    it('should return 404 for non-object ids',(done) => {
+      request(app)
+        .get(`/todos/123`)
+        .expect(404)
+        .end(done);
+    });
   })
+
+
 });
