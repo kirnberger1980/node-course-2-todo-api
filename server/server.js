4160ce25dbb97e36575dbc5511ff1d6fa922145a
Marcell Jobs <marcell.jobs@gmail.com>
2017-08-31 13:20:10 +0200

    Setup app for heroku

--- server/server.js
+++ server/server.js
@@ -9,6 +9,8 @@ var {Todo} = require('./models/todo.js');
 var {User} = require('./models/user.js');
 
 var app = express();
+// heroku
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
@@ -46,8 +48,8 @@ app.get('/todos/:id', (req,res) => {
   })
 });
 
-app.listen(3000, () => {
-  console.log('Started on port 3000');
+app.listen(port, () => {
+  console.log('Started on port',port);
 });
 
 module.exports = {app};
