cb1823e6f1c06e9067b6c5d57329bbc674e34ee4
Marcell Jobs <marcell.jobs@gmail.com>
2017-08-31 11:56:38 +0200

    add queries playground

---
+++ playground/mongoose-queries.js
@@ -0,0 +1,37 @@
+const {ObjectID} = require('mongodb');
+
+const {mongoose} = require('./../server/db/mongoose.js');
+const {Todo} = require('./../server/models/todo.js');
+const {User} = require('./../server/models/user.js')
+
+var id = '59a6b32fd663475856033ac6';
+
+// if (!ObjectID.isValid(id)) {
+//   console.log('ID not valid');
+// }
+
+// Todo.find({
+//   _id: id
+// }).then((todos) =>{
+//   console.log('Todos', todos);
+// });
+//
+// Todo.findOne({
+//   _id: id
+// }).then((todo) =>{
+//   console.log('Todo', todo);
+// });
+
+// Todo.findById(id).then((todo) =>{
+//   if (!todo) {
+//     return console.log('Document with id not found.');
+//   }
+//   console.log('Todo by id.', todo);
+// }).catch((e) => console.log(e));
+
+User.findById(id).then((user) => {
+  if (!user) {
+    return console.log(`User with id ${id} not found.`);
+  }
+  console.log('User by id', user);
+}).catch((e) => console.log(e));
