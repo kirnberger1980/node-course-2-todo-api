b95f48c28c408f73ac0936ffa28f972092310557
Marcell Jobs <marcell.jobs@gmail.com>
2017-08-31 12:26:08 +0200

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
+var {ObjectID} = require('mongodb');
+
 // ES6 Destructuring
 var {mongoose} = require('./db/mongoose.js');
 var {Todo} = require('./models/todo.js');
@@ -29,7 +31,20 @@ app.get('/todos', (req,res) => {
   });
 });
 
-
+app.get('/todos/:id', (req,res) => {
+  var id = req.params.id;
+  if (!ObjectID.isValid(id)) {
+    res.status(404).send();
+  }
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      res.status(404).send();
+    }
+    res.status(200).send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  })
+});
 
 app.listen(3000, () => {
   console.log('Started on port 3000');
