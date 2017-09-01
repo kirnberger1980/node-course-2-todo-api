967651429788ce56c486399b00d2277a5ec197d3
Marcell Jobs <marcell.jobs@gmail.com>
2017-09-01 16:03:55 +0200

    Add PUT /todos/:id route

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
+var _ = require('lodash');
 var express = require('express');
 var bodyParser = require('body-parser');
-
 var {ObjectID} = require('mongodb');
 
 // ES6 Destructuring
@@ -61,6 +61,24 @@ app.delete('/todos/:id', (req,res) => {
   }).catch((e) => {
     res.status(400).send();
   });
+});
+
+app.put('/todos/:id',(req,res) => {
+  var id = req.params.id;
+  var body = _.pick(req.body,['text','completed']);
+
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+  Todo.findByIdAndUpdate(id, {$set :body}, {new: true}).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+    res.send({todo});
+  }).catch((e) => res.status(400).send());
 });
 
 app.listen(port, () => {
